366fbd46a46e2caa2e4516b2053f8a1772a18aeb
Nguyen Tung Lam <tunglam20132208@gmail.com>
2019-10-01 09:47:09 +0700

    modules

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require("http");
+const time = require("./src/time");
 
 http
   .createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World");
+    res.end("Hello World" + time.date());
   })
   .listen(8080);
