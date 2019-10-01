8e819d3487c6144a5622e31d6390be51e9473541
Nguyen Tung Lam <tunglam20132208@gmail.com>
2019-10-01 09:52:36 +0700

    http module

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const http = require("http");
+const url = require("url");
 const time = require("./src/time");
 
 http
   .createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World" + time.date());
+    const { year, month } = url.parse(req.url, true).query;
+    res.end("year " + year + " month " + month);
   })
   .listen(8080);
+
+//localhost:8080/?year=2019&month=July
